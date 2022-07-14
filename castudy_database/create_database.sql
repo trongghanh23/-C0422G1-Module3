85f2d089f51ab7ef4bd469e25f475d8d764e1bfa
trongghanh23 <truongggh1602@gmail.com>
2022-07-15 05:36:17 +0700

    nộp bài tập

--- castudy_database/create_database.sql
+++ castudy_database/create_database.sql
@@ -1,109 +1,128 @@
 drop database castudy;
 create database if not exists  castudy;
+use castudy;
 
-CREATE TABLE vi_tri (
-    ma_vi_tri INT,
-    ten_vi_tri VARCHAR(45),
-    PRIMARY KEY (ma_vi_tri)
-);
-
-CREATE TABLE trinh_do (
-    ma_trinh_do INT,
-    ten_trinh_do VARCHAR(45),
-    PRIMARY KEY (ma_trinh_do)
+create table vi_tri (
+    ma_vi_tri int,
+    ten_vi_tri varchar(45),
+    primary key (ma_vi_tri)
 );
 
-CREATE TABLE bo_phan (
-    ma_bo_phan INT,
-    ten_bo_phan VARCHAR(45),
-    PRIMARY KEY (ma_bo_phan)
-);
-CREATE TABLE nhan_vien (
-    ma_nhan_vien INT AUTO_INCREMENT,
-    ho_ten VARCHAR(45),
-    ngay_sinh DATE,
-    so_cmnd VARCHAR(45),
-    luong DOUBLE,
-    so_dien_thoai VARCHAR(45),
-    email VARCHAR(45),
-    dia_chi VARCHAR(45),
-    ma_vi_tri INT,
-    ma_trinh_do INT,
-    ma_bo_phan INT,
-    PRIMARY KEY (ma_nhan_vien)
+create table trinh_do (
+    ma_trinh_do int,
+    ten_trinh_do varchar(45),
+    primary key (ma_trinh_do)
 );
 
-CREATE TABLE khach_hang (
-    ma_khach_hang INT AUTO_INCREMENT,
-    ma_loai_khach INT,
-    ho_ten VARCHAR(45),
-    ngay_sinh DATE,
-    gioi_tinh BIT(1),
-    so_cmnd VARCHAR(45),
-    so_dien_thoai VARCHAR(45),
-    email VARCHAR(45),
-    dia_chỉ VARCHAR(45),
-    PRIMARY KEY (ma_khach_hang)
+create table bo_phan (
+    ma_bo_phan int,
+    ten_bo_phan varchar(45),
+    primary key (ma_bo_phan)
+);
+create table nhan_vien (
+    ma_nhan_vien int auto_increment,
+    ho_ten varchar(45),
+    ngay_sinh date,
+    so_cmnd varchar(45),
+    luong double,
+    so_dien_thoai varchar(45),
+    email varchar(45),
+    dia_chi varchar(45),
+    ma_vi_tri int,
+    ma_trinh_do int,
+    ma_bo_phan int,
+    primary key (ma_nhan_vien),
+    foreign key(ma_vi_tri)references vi_tri(ma_vi_tri),
+    foreign key(ma_trinh_do)references trinh_do(ma_trinh_do),
+    foreign key(ma_bo_phan)references bo_phan(ma_bo_phan)
+    
+    
 );
 
-CREATE TABLE loai_khach (
-    ma_loai_khach INT,
-    ten_loai_khach VARCHAR(45),
-    PRIMARY KEY (ma_loai_khach)
+create table loai_khach (
+    ma_loai_khach int,
+    ten_loai_khach varchar(45),
+    primary key (ma_loai_khach)
 );
 
-CREATE TABLE loai_dich_vu (
-    ma_loai_dich_vu INT,
-    ten_loai_dich_vu VARCHAR(45),
-    PRIMARY KEY (ma_loai_dich_vu)
+
+create table khach_hang (
+    ma_khach_hang int auto_increment,
+    ma_loai_khach int,
+    ho_ten varchar(45),
+    ngay_sinh date,
+    gioi_tinh bit(1),
+    so_cmnd varchar(45),
+    so_dien_thoai varchar(45),
+    email varchar(45),
+    dia_chỉ VArchar(45),
+    PRimary kEy (ma_khach_hang),
+    foreign key (ma_loai_khach) references loai_khach(ma_loai_khach)
 );
 
-CREATE TABLE hop_dong (
-    ma_hop_dong INT AUTO_INCREMENT,
-    ngay_lam_hop_dong DATETIME,
-    ngay_ket_thuc DATETIME,
-    tien_dat_coc DOUBLE,
-    ma_nhan_vien INT,
-    ma_khach_hang INT,
-    ma_dich_vu INT,
-    PRIMARY KEY (ma_hop_dong)
+
+create table loai_dich_vu (
+    ma_loai_dich_vu int,
+    ten_loai_dich_vu varchar(45),
+    primary key (ma_loai_dich_vu)
 );
 
-CREATE TABLE dich_vu (
-    ma_dich_vu INT AUTO_INCREMENT,
-    ten_dich_vu VARCHAR(45),
-    dien_tich INT,
-    chi_phi_thue DOUBLE,
-    so_nguoi_toi_da INT,
-    ma_kieu_thue INT,
-    ma_loại_dich_vu INT,
-    tieu_chuan_phong VARCHAR(45),
-    ma_ta_tien_nghi_khac VARCHAR(45),
-    dien_tich_ho_boi DOUBLE,
-    so_tang INT,
-    dich_vu_mien_phi_di_kem TEXT,
-    PRIMARY KEY (ma_dich_vu)
+create table kieu_thue (
+    ma_kieu_thue int,
+    ten_kieu_thue varchar(45),
+    primary key (ma_kieu_thue)
 );
 
-CREATE TABLE kieu_thue (
-    ma_kieu_thue INT,
-    ten_kieu_thue VARCHAR(45),
-    PRIMARY KEY (ma_kieu_thue)
+create table dich_vu (
+    ma_dich_vu int auto_increment,
+    ten_dich_vu varchar(45),
+    dien_tich int,
+    chi_phi_thue double,
+    so_nguoi_toi_da int,
+    ma_kieu_thue int,
+    ma_loai_dich_vu int,
+    tieu_chuan_phong varchar(45),
+    ma_ta_tien_nghi_khac varchar(45),
+    dien_tich_ho_boi double,
+    so_tang int,
+    dich_vu_mien_phi_di_kem text,
+    primary key (ma_dich_vu),
+    foreign key(ma_loai_dich_vu) references  loai_dich_vu(ma_loai_dich_vu),
+    foreign key(ma_kieu_thue) references  kieu_thue(ma_kieu_thue)
 );
 
-CREATE TABLE hop_dong_chi_tiet (
-    ma_hop_dong_chi_tiet INT,
-    ma_hop_dong INT,
-    ma_dich_vu_di_kem INT,
-    so_luong INT,
-    PRIMARY KEY (ma_hop_dong_chi_tiet)
+
+create table hop_dong (
+    ma_hop_dong int auto_increment,
+    ngay_lam_hop_dong datetime,
+    ngay_ket_thuc datetime,
+    tien_dat_coc double,
+    ma_nhan_vien int,
+    ma_khach_hang int,
+    ma_dich_vu int,
+    primary key (ma_hop_dong),
+    foreign key(ma_nhan_vien) references nhan_vien(ma_nhan_vien),
+    foreign key(ma_khach_hang) references khach_hang(ma_khach_hang),
+    foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
 );
 
-CREATE TABLE dich_vu_di_kem (
-    ma_dich_vu_di_kem INT,
-    ten_dich_vu_di_kem VARCHAR(45),
-    gia DOUBLE,
-    don_vi VARCHAR(10),
-    trang_thai VARCHAR(45),
-    PRIMARY KEY (ma_dich_vu_di_kem)
+create table dich_vu_di_kem (
+    ma_dich_vu_di_kem int,
+    ten_dich_vu_di_kem varchar(45),
+    gia double,
+    don_vi varchar(10),
+    trang_thai varchar(45),
+    primary key (ma_dich_vu_di_kem)
 );
+
+create table hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet int,
+    ma_hop_dong int,
+    ma_dich_vu_di_kem int,
+    so_luong int,
+    primary key (ma_hop_dong_chi_tiet),
+    foreign key(ma_hop_dong) references hop_dong(ma_hop_dong),
+    foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem)
+);
+
+
